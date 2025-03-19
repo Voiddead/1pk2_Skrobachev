cd3978775d0ca6f3c694963f0e5bbd6cd22537c5
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 22:31:24 +0500

    выполнено 5 задание 3 урока

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод диапазона и шага
+        Console.Write("Введите начальную температуру в градусах Цельсия: ");
+        double start = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите конечную температуру в градусах Цельсия: ");
+        double end = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите шаг изменения температуры: ");
+        double step = double.Parse(Console.ReadLine());
+
+        // Вывод заголовка таблицы
+        Console.WriteLine("°C\t°F");
+        Console.WriteLine("------------");
+
+        // Вывод таблицы
+        for (double celsius = start; celsius <= end; celsius += step)
+        {
+            double fahrenheit = celsius * 1.8 + 32; // Формула перевода
+            Console.WriteLine($"{celsius}\t{fahrenheit}");
+        }
+    }
+}
