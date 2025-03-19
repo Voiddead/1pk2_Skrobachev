612a94b3ea2927ca6bba2e8297a7be29c4fd8637
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 22:39:00 +0500

    выполнено 7 задание 3 урока

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double g = 9.8; // ускорение свободного падения, м/с^2
+        double t = 0.0; // время, с
+        double dt = 0.5; // шаг времени, с
+
+        Console.WriteLine("t\tv");
+        Console.WriteLine("-" + new string('-', 13));
+
+        while (t <= 10.0)
+        {
+            double v = g * t;
+            Console.WriteLine($"{t:F1}\t{v:F1}");
+            t += dt;
+        }
+    }
+}
