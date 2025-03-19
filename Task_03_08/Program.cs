d5dc5d0fdbd0615ea6aad10f679c09c327911a11
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 22:44:28 +0500

    выполнено 8 задание 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Числа, которые делятся на 3, но не делятся на 5:");
+
+        for (int i = 20; i <= 50; i++)
+        {
+            if (i % 3 == 0 && i % 5 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
