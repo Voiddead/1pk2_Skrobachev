fae35d6bf614095805202991493933cc385584b4
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 22:36:34 +0500

    выполнено 6 задание 3 урока

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = -4;
+        double h = 0.5;
+
+        Console.WriteLine("x\t|x|");
+        Console.WriteLine("-" + new string('-', 13));
+
+        while (x <= 4)
+        {
+            double y = Math.Abs(x);
+            Console.WriteLine($"{x:F1}\t{y:F1}");
+            x += h;
+        }
+    }
+}
