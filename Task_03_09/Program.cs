1453c7a3c447078cba6593be6ea164a23c2f747d
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 22:47:39 +0500

    выполнено 9 задание 3 урока

---
+++ Task_03_09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Входные данные
+        double x = 100; // Начальный вклад
+        double p = 10; // Процент увеличения ежегодно
+        double y = 200; // Целевой вклад
+
+        int years = 0;
+        while (x < y)
+        {
+            x += x * (p / 100); // Увеличиваем вклад на p%
+            x = Math.Truncate(x); // Отбрасываем дробную часть копеек
+            years++;
+        }
+
+        Console.WriteLine($"Через {years} лет вклад составит не менее {y} рублей.");
+    }
+}
