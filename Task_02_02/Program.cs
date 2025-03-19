fb6f75db4817d168128cafe12f2696e803f4a1df
Степан Скробачев <stepanskra@gmail.com>
2025-03-19 21:00:54 +0500

    выполнена 2 задача 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Заданные значения переменных
+        double a = 8;
+        double b = 14;
+        double c = Math.PI / 4; // c = π/4
+
+        // Вычисление значения выражения
+        double expressionValue = (4 / b) + (3 / a) - 1;
+        double sinC = Math.Sin(c);
+        double tgC = Math.Tan(c);
+        double denominator = Math.Abs(a - b) * (sinC + tgC);
+
+        // Итоговое значение
+        double result = expressionValue / denominator;
+
+        // Вывод результата
+        Console.WriteLine("Результат: " + result);
+    }
+}
